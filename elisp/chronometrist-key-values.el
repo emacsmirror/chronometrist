a6c02aaf804b8f78816cb548c3423d2c9104d76b
contrapunctus <contrapunctus@disroot.org>
2021-06-01 17:15:17 +0530

    fix(distribution): ship Elisp file; remove literate-elisp dependency

--- elisp/chronometrist-key-values.el
+++ elisp/chronometrist-key-values.el
@@ -1,28 +1,3 @@
-;;; chronometrist-key-values.el --- add key-values to Chronometrist data -*- lexical-binding: t; -*-
-
-;; Author: contrapunctus <xmpp:contrapunctus@jabber.fr>
-;; Maintainer: contrapunctus <xmpp:contrapunctus@jabber.fr>
-;; Keywords: calendar
-;; Homepage: https://tildegit.org/contrapunctus/chronometrist
-;; Package-Requires: ((chronometrist "0.7.0"))
-;; Version: 0.1.0
-
-;; This is free and unencumbered software released into the public domain.
-;;
-;; Anyone is free to copy, modify, publish, use, compile, sell, or
-;; distribute this software, either in source code form or as a compiled
-;; binary, for any purpose, commercial or non-commercial, and by any
-;; means.
-;;
-;; For more information, please refer to <https://unlicense.org>
-
-;;; Commentary:
-;;
-;; This package lets users attach tags and key-values to their tracked time, similar to tags and properties in Org mode.
-;;
-;; To use, add one or more of these functions to any chronometrist hook except `chronometrist-before-in-functions`.
-;; * `completing-read'-based - `chronometrist-tags-add` and/or `chronometrist-kv-add'
-;; * `choice'-based (Hydra-like) - `chronometrist-unified-choice'
 
 ;;; Code:
 (require 'literate-elisp)
